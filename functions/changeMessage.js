1db8b10a46522a6f79661b90f9b4454d48fd3d27
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-09-24 23:56:48 +0200

    Done with change/update message function

--- functions/changeMessage.js
+++ functions/changeMessage.js
@@ -1,8 +1,36 @@
-// const dynamoDb = require("../dynamoClient.js");
-// const response = require("../responseCodes.js");
+const dynamoDb = require("../dynamoClient.js");
+const response = require("../responseCodes.js");
 
-// exports.module.changeMessange = async (event) => {
-//   const params = {
-//     TableName: "Shui-border",
-//   };
-// };
+module.exports.changeMessage = async (event) => {
+  const { shuiId, newText } = JSON.parse(event.body);
+  console.log(`Your id: ${shuiId} and this is what newText is: ${newText}`);
+
+  const params = {
+    TableName: "Shui-border",
+    Key: {
+      shuiId: shuiId,
+    },
+    UpdateExpression: "set #text = :newText",
+    ExpressionAttributeValues: {
+      ":newText": newText,
+    },
+    ExpressionAttributeNames: {
+      "#text": "text",
+    },
+    ReturnValues: "UPDATED_NEW",
+  };
+
+  try {
+    console.log(`Your id: ${shuiId} and this is what newText is: ${newText}`);
+    const result = await dynamoDb.update(params).promise();
+    return response.success({
+      message: "Message updated successfully",
+      data: result.Attributes,
+    });
+  } catch (error) {
+    console.error(error);
+    return response.error({
+      message: "An error occured while updating message",
+    });
+  }
+};
